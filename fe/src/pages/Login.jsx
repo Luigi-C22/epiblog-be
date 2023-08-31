3646534725d7497089b0079ab4ae1e682c00c89f
Luigi <lc71@libero.it>
2023-08-31 18:12:17 +0200

    aggiorno

--- fe/src/pages/Login.jsx
+++ fe/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import  axios  from 'axios';
+import axios from 'axios';
 
 
 const Login = () => {
@@ -11,40 +11,41 @@ const Login = () => {
 
         await axios
             .post("http://localhost:5050/login", loginFormData)
-            .then((res) => console.log(res));
-                //ocalStorage.setItem('userLoggedIn', JSON.stringify(res.token))
-    };
-    return (
-        <div className="flex flex-col justify-center items-center">
-            <form
-                onSubmit={onSubmit}
-                className='flex flex-col gap-3 justify-center items-center'
-            >
-                <input
-                    className="p-2 bg-zinc-100 text-black rounded"
-                    type="email"
-                    name='email'
-                    onChange={(e) => setLoginFormData({
-                        ...loginFormData,
-                        email: e.target.value,
-                    })}
-                />
+            .then(res) => {
+    localStorage.setItem('userLoggedIn', JSON.stringify(res.data.token));
+};
+};
+return (
+    <div className="flex flex-col justify-center items-center">
+        <form
+            onSubmit={onSubmit}
+            className='flex flex-col gap-3 justify-center items-center'
+        >
+            <input
+                className="p-2 bg-zinc-100 text-black rounded"
+                type="email"
+                name='email'
+                onChange={(e) => setLoginFormData({
+                    ...loginFormData,
+                    email: e.target.value,
+                })}
+            />
 
-                <input
-                    className="p-2 bg-zinc-100 text-black rounded"
-                    type="password"
-                    name='password'
-                    onChange={(e) => setLoginFormData({
-                        ...loginFormData,
-                        password: e.target.value,
-                    })
-                    }
-                />
-                <button type='submit'>Login</button>
-            </form>
+            <input
+                className="p-2 bg-zinc-100 text-black rounded"
+                type="password"
+                name='password'
+                onChange={(e) => setLoginFormData({
+                    ...loginFormData,
+                    password: e.target.value,
+                })
+                }
+            />
+            <button type='submit'>Login</button>
+        </form>
 
-        </div>
-    );
+    </div>
+);
 }
 
 export default Login;
